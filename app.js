17ccfeed18fd5e7899c4684e876e39c85844b5f1
Syed Rafay <rafaysyed637@gmail.com>
2022-11-13 23:51:07 +0500

    done

---
+++ app.js
@@ -0,0 +1,100 @@
+// Chapter 1 start
+alert ("Welcome to my website");
+alert ("Error! please enter a valid password");
+alert ("Welcome To JS Land..." + "\n" + "Happy Coding!")
+alert ("Welcome To JS Land...");
+alert ("Happy Coding!" + "\n" + "Prevent this page from creating additional dialogues");
+alert ("Hello... I can run JS through my web browser's console");
+// Chapter 1 end
+
+// Chapter 2 start
+var username = "a";
+var myname = "Syed Rafay";
+
+var message = "Hello World";
+alert(message);
+
+var stbio1 = "Umar";
+var stbio2 = "15 years old";
+var stbio3 = "Certified Mobile Application Development";
+alert(stbio1);
+alert(stbio2);
+alert(stbio3);
+
+var food = "pizza";
+alert("pizza" + "\n" + "pizz" + "\n" + "piz" + "\n" + "pi" + "\n" + "p");
+
+var email = "My email address is rafaysyed637@gmail.com";
+alert(email);
+
+var book = "I am trying to learn from the book A Smarter Way To Learn JavaScript";
+alert(book);
+
+var script = "Yah! I can write HTML content through JavaScript";
+alert(script);
+
+var design = "---------- SIOS ----------";
+alert(design);
+// Chapter 2 end
+
+// Chapter 3 start
+var age = "I am 15 years old";
+alert(age);
+
+var times = "You have visited this site 14 times.";
+alert(times);
+
+var BirthYear = "My birth year is 1990" + "\n" + "Data type of my declared variable is number";
+alert(BirthYear);
+
+var clothes = "John Ducket" + "\n" + "J." + "\n" + "Ordered 5 T-shirt(s)";
+alert(clothes);
+// Chapter 3 end
+
+// Chapter 4 start
+var ab = "how";
+var bc = " are ";
+var ca = "you??";
+var n = ab + bc + ca;
+alert(n);
+
+var LegalVariable;
+var abc;
+var ab12;
+var _abc;
+var _abc12;
+var _abc;
+
+var IllegalVariable;
+// var 123;
+// var 12ab;
+// var alert;
+// var @bc;
+// var -abc;
+
+console.log("$my" + "\n" + "_" + "\n" + "1 variable");
+console.log("$name" + "\n" + "_name" + "\n" + "name");
+console.log("variable names are case sensitive");
+console.log("variable names should not be JS keywords");
+// Chapter 4 end
+
+// Chapter 5 start
+var no1 = 3;
+var no2 = 5;
+var sumOfno = 8;
+alert(sumOfno);
+
+var a = 600;
+var b = 5;
+var c = b * a;
+alert("Total cost to buy 5 tickets to a movie is " + c + "PKR");
+
+var d = 10;
+var e = 104.80;
+var f = d * e;
+var g = 25;
+var h = 28;
+var i = g * h;
+var j = f + i;
+alert("Total Currency in PKR: " + j)
+// Chapter 5 end
